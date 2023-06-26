6675ac586873dc24e4f9dbfb90f9c1325ed8a24b
rivaldo-bogar <androsbgr13@gmail.com>
2023-06-26 13:38:09 +0800

    feat : make archivement and adding new file archivement.js

--- src/componentJavaScript/Comright.js
+++ src/componentJavaScript/Comright.js
@@ -1,9 +1,11 @@
 import'./component_css/Component.css'
 import Introtext from './Introtext';
+import Archivement from './Archivement'
 function Boxright() {
     return (
 <div className="kotakkanan">
 <Introtext/>
+<Archivement/>
 </div>
     )
 };
---
+++ src/componentJavaScript/archivement.js
@@ -0,0 +1,25 @@
+import Learncodebed from '../template/Learncodebed.png'
+import Blenderbed from '../template/Blenderpaper.png'
+import Gamedev from '../template/Gamedevpaper.png'
+
+function Archivement() {
+    return (
+        <div className='bugsolve'>
+            <h1 className='textarchivement'>Archivement</h1>
+<div className='bingkai'>
+<img src={Blenderbed} alt="gambar tidak tersedia" className="codepaper"/>
+
+</div>
+
+<div className='bingkai'>
+<img src={Learncodebed} alt="gambar tidak tersedia" className="codepaper"/>
+</div>
+        <div className='bingkai'>
+<img src={Gamedev} alt="gambar tidak tersedia" className="codepaper"/>
+        </div>
+</div>
+
+
+    )
+}
+export default Archivement;
